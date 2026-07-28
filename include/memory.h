1acfd68fff9eb8051f94320b9f553216c337528e
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-07-28 06:07:03 +0000

    Add Functions declarations

--- include/memory.h
+++ include/memory.h
@@ -1,11 +1,12 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include "shell_types.h"
+
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 void *shell_malloc(size_t size);
 
@@ -14,5 +15,9 @@ void *shell_realloc(void *ptr, size_t size);
 void shell_free(void *ptr);
 
 size_t memory_allocated_bytes(void);
+
+size_t memory_allocated_count(void);
+
+size_t memory_freed_count(void);
 
 #endif // !MEMORY_H
