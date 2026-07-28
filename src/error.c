f3c1f08585cef05d19dc431814469faae2a7e753
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-07-28 11:00:33 +0000

    Add set_error function

---
+++ src/error.c
@@ -0,0 +1,19 @@
+#include "../include/error.h"
+#include <string.h>
+
+static ErrorCode current_error; // stores the current error code
+
+static char error_buffer[256]; // stores the current error message
+
+void set_error(ErrorCode code, const char *message) {
+    if (message == NULL) {
+        error_buffer[0] = '\0';
+        return;
+    }
+
+    current_error = code;
+
+    strcpy(error_buffer, message);
+
+    return;
+}
