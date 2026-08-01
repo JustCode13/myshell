16a59459894a5ba2573d3f23e4a64e28210c4e01
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-01 06:34:49 +0000

    Add explanation commits for each function

--- include/prompt.h
+++ include/prompt.h
@@ -4,12 +4,26 @@
 #include "shell.h"
 #include <stddef.h>
 
+#define INITIAL_INPUT_SIZE 64
+
 int prompt_initialize(ShellContext *ctx);
+// It initializes the prompt only field with default character '>' to display
+// if error returns -1
 
 int prompt_read_line(char **buffer, size_t *length);
+// reads a line, character by character with maxium INITIAL_INPUT_SIZE
+// characters, and stores the length in length and read line in buffer if error
+// returns -1
 
 void prompt_update(ShellContext *ctx);
+// gets currnt_working_directory with getcwd and updates prompt and store cwd in
+// ctx cwd if error just returns
+
+int test_rc(char *output);
+// just a normal function to test read_characters
+// not use of this function WORTHLESS
 
 const char *prompt_get(const ShellContext *ctx);
+// return the prompt from ctx, but you can't update it
 
 #endif // !PROMPT_H
