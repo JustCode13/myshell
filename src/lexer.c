955cd3b837083b8f93c68445c6ff1ad88f6b0bcf
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-02 05:51:46 +0000

    Add lex_word function

--- src/lexer.c
+++ src/lexer.c
@@ -2,6 +2,90 @@
 #include "../include/string_builder.h"
 
 #include <ctype.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <string.h>
 
+static bool is_operator_char(char c) {
+    switch (c) {
+    case '&':
+        return true;
 
+    case '|':
+        return true;
+
+    case '>':
+        return true;
+
+    case '<':
+        return true;
+
+    case ';':
+        return true;
+
+    case '(':
+        return true;
+
+        return true;
+    case '{':
+
+    default:
+        return false;
+    }
+}
+
+static int append_token(Lexer *lexer, TokenType Type, const char *text,
+                        size_t length);
+
+// static int lex_word(Lexer *lexer) {
+//     if (lexer == NULL) {
+//         return -1;
+//     }
+//
+//     char next_char = lexer->input[lexer->position];
+//     char *first_char_add = &lexer->input[lexer->position];
+//     size_t length = 0;
+//
+//     while (next_char != " " || is_operator_char(next_char) != 0) {
+//         length++;
+//         next_char = lexer->input[lexer->position++];
+//     }
+//
+//     const char *text[length];
+//
+//     for (size_t i = 0; i < length; i++) {
+//         size_t current_position = lexer->position - length;
+//         char current_char = lexer->input[current_position];
+//         text[i] = &current_char;
+//         current_position++;
+//     }
+//
+//     if (append_token(lexer, TOKEN_WORD, &text, length) != 0) {
+//         return -1;
+//     }
+//
+//     return 0;
+// }
+
+static int lex_word(Lexer *lexer) {
+    if (lexer == NULL || lexer->input == NULL) {
+        return -1;
+    }
+
+    size_t start_position = lexer->position;
+    const char *start_address = lexer->input + lexer->position;
+
+    while (lexer->input[lexer->position] != '\0' ||
+           !isspace((unsigned char)lexer->input[lexer->position]) ||
+           !is_operator_char(lexer->input[lexer->position])) {
+        lexer->position++;
+    }
+
+    size_t length = lexer->position - start_position;
+
+    if (append_token(lexer, TOKEN_WORD, start_address, length) != 0) {
+        return -1;
+    }
+
+    return 0;
+}
