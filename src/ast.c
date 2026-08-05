f4861ccbdec5aec04e3235175099e2f78e97aca3
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-05 06:52:13 +0000

    Add static print_indent function

--- src/ast.c
+++ src/ast.c
@@ -3,19 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void destroy_redirects(Redirect *head) {
-    if (head == NULL) {
-        return;
+// static void destroy_redirects(Redirect *head) {
+//     if (head == NULL) {
+//         return;
+//     }
+//
+//     Redirect *current = head;
+//
+//     while (current != NULL) {
+//         Redirect *next = current->next;
+//
+//         free(current->target);
+//         free(current);
+//
+//         current = next;
+//     }
+// }
+
+static void print_indent(int depth) {
+    for (int counter = 0; counter < depth; counter++) {
+        printf(" ");
     }
+}
 
-    Redirect *current = head;
-
-    while (current != NULL) {
-        Redirect *next = current->next;
-
-        free(current->target);
-        free(current);
-
-        current = next;
-    }
+int use_indent(int depth) {
+    print_indent(depth);
+    return 0;
 }
