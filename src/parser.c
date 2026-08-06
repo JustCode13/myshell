80a8709905ecf567591037e427398198e5f81322
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-06 05:36:57 +0000

    Add parser_initialize function

--- src/parser.c
+++ src/parser.c
@@ -3,8 +3,18 @@
 
 #include <stdlib.h>
 
-static ASTNode *parse_sequence(Parser *parser) {
-    if (parser == NULL) {
-        return NULL;
+// static ASTNode *parse_sequence(Parser *parser) {
+//     if (parser == NULL) {
+//         return NULL;
+//     }
+//
+// }
+//
+
+int parser_initialize(Parser *parser, const Lexer *lexer) {
+    if (parser == NULL || lexer == NULL) {
+        return -1;
     }
+
+    return 0;
 }
