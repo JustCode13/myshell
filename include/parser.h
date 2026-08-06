69602fcfa6a6f0e71a1fdd6b786f26e0c2d06f9e
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-06 04:13:43 +0000

    Add headers, struct and declarations

---
+++ include/parser.h
@@ -0,0 +1,24 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "lexer.h"
+#include "shell_types.h"
+#include <stddef.h>
+
+typedef struct Parser {
+    const Lexer *lexer;
+
+    size_t current;
+
+    bool panic_mode;
+} Parser;
+
+ASTNode *parser_parse(Parser *parser);
+
+int parser_initialize(Parser *parser, const Lexer *lexer);
+
+void parser_destroy(Parser *parser);
+
+const char *parser_last_error(const Parser *parser);
+
+#endif // !PARSER_H
