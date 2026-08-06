6983e948c9e61f72f2c69cb066e4d5ab7943d69b
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-06 05:08:18 +0000

    Add static parser_sequence input validation

---
+++ src/parser.c
@@ -0,0 +1,10 @@
+#include "../include/parser.h"
+#include "../include/ast.h"
+
+#include <stdlib.h>
+
+static ASTNode *parser_sequence(Parser *parser) {
+    if (parser == NULL) {
+        return NULL;
+    }
+}
