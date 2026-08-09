15b1af588d27ca8d186e9212f582b5a49d459e34
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-09 14:56:34 +0000

    Add test_parser and initialize lexer, parser

---
+++ tests/test_parser.c
@@ -0,0 +1,12 @@
+#include "../include/ast.h"
+#include "../include/test.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int test_parser(void) {
+    const char *input = "echo hello";
+
+    Lexer lexer = {0};
+    Parser parser = {0};
+}
