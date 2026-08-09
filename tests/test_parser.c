da1ff2c2c7dbde8d81e65505920a69a9332a05e0
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-09 15:05:35 +0000

    Initialize lexer properties

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -5,8 +5,14 @@
 #include <stdlib.h>
 
 int test_parser(void) {
-    const char *input = "echo hello";
+
+    const char *input = "echo hello world";
 
     Lexer lexer = {0};
     Parser parser = {0};
+
+    if (lexer_initialize(&lexer, input) != 0) {
+        fprintf(stderr, "Lexer Initialize Failed\n");
+        return EXIT_FAILURE;
+    }
 }
