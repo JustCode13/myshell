3a16cfa5b0f5b4dc17ad1c39ae464fc4d1b3e764
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-10 07:41:40 +0000

    Add comments for functions

--- include/parser.h
+++ include/parser.h
@@ -17,10 +17,18 @@ typedef struct Parser {
 
 ASTNode *parser_parse(Parser *parser);
 
+//
+
 int parser_initialize(Parser *parser, const Lexer *lexer);
+
+// initializes the parse with default values and lexer
 
 void parser_destroy(Parser *parser);
 
+//
+
 const char *parser_last_error(const Parser *parser);
+
+// returns the last parser error
 
 #endif // !PARSER_H
