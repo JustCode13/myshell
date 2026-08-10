a852e17f94165f4e0c97af696e32e4ec12856f69
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-10 07:59:20 +0000

    Add ParserTest struct

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -1,23 +1,10 @@
 #include "../include/ast.h"
-#include "../include/test.h"
+#include "../include/parser.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int test_parser(void) {
-
-    const char *input = "echo hello world";
-
-    Lexer lexer = {0};
-    Parser parser = {0};
-
-    if (lexer_initialize(&lexer, input) != 0) {
-        fprintf(stderr, "Lexer Initialize Failed\n");
-        return EXIT_FAILURE;
-    }
-
-    if (lexer_tokenize(lexer) != 0) {
-        fprintf(stderr, "Lexer Tokenization Failed\n");
-        return EXIT_FAILURE;
-    }
-}
+typedef struct {
+    const char *input;
+    const char *description;
+} ParserTest;
