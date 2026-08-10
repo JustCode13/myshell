30290151236b6ac217f749f591d045dce234bd38
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-10 08:01:22 +0000

    Add static test_result function

--- tests/test_ast.c
+++ tests/test_ast.c
@@ -6,3 +6,14 @@
 
 static int tests_run = 0;
 static int tests_passed = 0;
+
+static void test_result(const char *name, bool passed) {
+    tests_run++;
+
+    if (passed) {
+        tests_passed++;
+        printf("[PASS] %s\n", name);
+    } else {
+        printf("[FAIL] %s\n", name);
+    }
+}
