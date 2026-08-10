0630d1f6377ef1ff6556004be0dc851a1a9dee32
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-10 07:41:57 +0000

    Add functions declarations

--- include/test.h
+++ include/test.h
@@ -3,8 +3,13 @@
 #include <stddef.h>
 #include <stdio.h>
 
+#include "lexer.h"
+#include "parser.h"
+
 int test_memory(void);
 int test_prompt(void);
 int test_lexer(void);
+int test_parser(void);
+int test_ast(void);
 
 #endif // !TEST_H
