99320eac061fbbc119e773983938cec35c4addf8
codewithtea <262361023+JustCode13@users.noreply.github.com>
2026-08-11 04:52:01 +0000

    Add functions declarations

---
+++ include/redirect.h
@@ -0,0 +1,14 @@
+#ifndef REDIRECT_H
+#define REDIRECT_H
+
+#include "shell_types.h"
+
+int redirect_apply(Redirect *list);
+
+int redirect_store(void);
+
+int redirect_open_file(const Redirect *redir);
+
+void redirect_close_saved(void);
+
+#endif // !REDIRECT_H
